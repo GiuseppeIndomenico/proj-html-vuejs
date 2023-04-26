471780439d77b6dc690205488fc2f3fca158e9c6
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-26 16:57:36 +0200

    add section main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,14 +1,28 @@
 <template>
     <main class="mt-4 ps-5">
         <div class="d-flex">
-            <h3>OUR CONSULTANTS CAN HELP YOU</h3>
-            <div class="gi-tratto"></div>
+            <h3 class="title-main">OUR CONSULTANTS CAN HELP YOU</h3>
+            <div class="gi-tratto long"></div>
         </div>
         <div class="container">
             <div class="row">
                 <ConsultComponent v-for="cons in store.consultants" :title="cons.title" :p="cons.p" :icon="cons.icon" />
             </div>
         </div>
+        <section>
+            <div class="row">
+                <div class="col-7">
+                    <img src="\images\business-people-working-together-on-project-and-5FHSKBL.jpg" alt="">
+                </div>
+
+                <div class="col-5 d-flex align-items-center dark">
+                    <div class="gi-tratto short me-2"></div>
+                    <h3>ABOUT US</h3>
+                </div>
+
+            </div>
+        </section>
+
     </main>
 </template>
 
@@ -32,14 +46,32 @@ export default {
 
 h3 {
     font-family: 'Bai Jamjuree';
+
+}
+
+.title-main {
+    margin-left: 280px;
 }
 
 .gi-tratto {
     background-color: $sec;
     height: 3px;
-    width: 74%;
+
+}
+
+.long {
+    width: 59%;
     margin-top: 20px;
     margin-left: 10px;
+}
 
+.short {
+    width: 50px;
+    margin-bottom: 5px;
+}
+
+.dark {
+    background-color: $primary;
+    color: $light
 }
 </style>
