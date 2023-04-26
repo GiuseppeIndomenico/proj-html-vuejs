5a8a7d3fe57b62301ab32d8bb464a8df145e4612
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-26 15:10:45 +0200

    add jumbotron

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,26 @@
 <template>
     <header>
-
-        <nav class="container d-flex justify-content-between">
+        <img class="jumbo" src="\images\portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg" alt="">
+        <nav class="d-flex justify-content-between align-items-center">
             <div class="logo">
                 <img class="img-fluid" src="\images\logotype.png" alt="logo">
             </div>
             <ul class="d-flex align-items-center justify-content-center m-0">
                 <li v-for="(link, index) in store.links" :class="{ 'active': link.active }" @click="setActive(index)"
                     class="p-3 d-flex align-items-center">
-                    <a @click="setActive()" class="fw-semibold text-uppercase" :href="link.url">{{ link.title }}</a>
+                    <a @click="setActive()" class="text-uppercase" :href="link.url">{{ link.title }}</a>
                 </li>
             </ul>
         </nav>
+        <div class="title">
+            <div class="d-flex justify-content-start align-items-center mb-3">
+                <div class="gi-tratto"></div>
+                <h5>ALWATS THE BEST WAY YOU NEED IT</h5>
+            </div>
+            <h1>THE BEST BUSINESS CONSULTING</h1>
+            <button class="gi-button">READ MORE</button>
+
+        </div>
     </header>
 </template>
 
@@ -43,12 +52,81 @@ export default {
 <style lang="scss" scoped>
 @use'../assets/partials/variables.scss' as *;
 
+header {
+    width: 100%;
+    height: 100vh;
+    background-color: $primary;
 
-.active {
-    border-top: 3px inset $light;
+    .jumbo {
+        height: 100vh;
+        width: 70%;
+        object-fit: fill;
+        position: absolute;
+        right: 0;
+        z-index: 1;
+    }
 
-    a {
-        color: $light
+    nav {
+        width: 90%;
+        margin: auto;
+
+        .logo {
+            width: 130px;
+        }
+
+        a {
+            text-decoration: none;
+            color: $light;
+            opacity: 0.7;
+            z-index: 2;
+            background-color: transparent;
+            font-weight: 300;
+        }
+    }
+
+    .gi-tratto {
+        padding: 2px;
+        background-color: $sec;
+        width: 50px;
+        margin-bottom: 5px;
+    }
+
+    .title {
+        z-index: 10;
+        color: $light;
+        width: 40%;
+        position: absolute;
+        background-color: transparent;
+        top: 40%;
+        left: 5.5%;
+
+        h5 {
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 19px;
+            letter-spacing: 2.8px;
+            padding-left: 20px;
+        }
+
+        h1 {
+            font-size: 106px;
+            font-weight: 700;
+            line-height: 93px;
+            letter-spacing: -2px;
+            z-index: 1000000;
+        }
+    }
+
+    .active {
+        border-top: 3px inset white;
+        z-index: 2;
+        background-color: transparent;
+
+        a {
+            color: $light;
+            opacity: 1;
+            font-weight: bold;
+        }
     }
 }
 </style>
